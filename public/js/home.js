5c829dd5b5a1bb107c8872fe777833cdf2c99270
J03B <byucrazyfan@gmail.com>
2023-03-18 23:03:28 -0500

    Finish Add Goal form functionality with validations and backend database integration

--- public/js/home.js
+++ public/js/home.js
@@ -1,8 +1,8 @@
 // Grab HTML Elements
-const addCategoryBtn = document.getElementById('add-category-btn');
-const categoryNameInput = document.getElementById('category-name');
-const submitCategoryBtn = document.getElementById('submit-category-btn');
-const addGoalBtn = document.getElementById('add-goal-btn');
+// const addCategoryBtn = document.getElementById('add-category-btn');
+// const categoryNameInput = document.getElementById('category-name');
+// const submitCategoryBtn = document.getElementById('submit-category-btn');
+// const addGoalBtn = document.getElementById('add-goal-btn');
 const goalNav = document.getElementById('goal-nav-list');
 const accordionContainer = document.getElementById('accordion');
 let goalNavItems;
@@ -130,30 +130,6 @@ async function displayGoals(category, userData) {
   }
 }
 
-// Submit category
-const submitCategory = async (event) => {
-  console.log('Attempting to add a category');
-  event.preventDefault();
-
-  const categoryName = categoryNameInput.value.trim();
-  
-  if (categoryName) {
-    const response = await fetch('/api/categories', {
-      method: 'POST',
-      body: JSON.stringify({ categoryName }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    // Clear category name input
-    categoryNameInput.value = '';
-    if (response.ok) {
-      refreshPage('All Goals');
-    } else {
-      alert('Category could not be added.');
-    }
-  }
-};
-
 // Refreshes user data, nav bar, and goals
 async function refreshPage(goalCategory) {
   let userData = await getData()
@@ -171,28 +147,6 @@ async function refreshPage(goalCategory) {
     });
   });
 }
-
-// Event listener to add a new goal
-const addGoalHandler = async (e) => {
-  e.preventDefault();
-
-  const response = await fetch(`add-goal`, {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json', },
-  });
-  
-  if (response.ok) {
-    window.location.href = '/add-goal';
-  }
-  else {
-    alert('Button did not work');
-  }
-};
-addGoalBtn
-.addEventListener('click', addGoalHandler);
-
-submitCategoryBtn
-  .addEventListener('click', submitCategory);
 
 if (window.location.pathname == "/") {
   refreshPage('All Goals');
